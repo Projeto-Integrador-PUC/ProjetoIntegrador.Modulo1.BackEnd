79b26a708d6be54485eca04e6ebad54c9591a224
Leonardo Couto Molinar Henrique <leo_couto29@hotmail.com>
2023-07-05 18:08:36 -0300

    Definindo segurança do cookie

--- ProjetoIntegrador.Modulo1.BackEnd/Extensions/Extensions.cs
+++ ProjetoIntegrador.Modulo1.BackEnd/Extensions/Extensions.cs
@@ -64,6 +64,7 @@ namespace ProjetoIntegrador.Modulo1.BackEnd.Extensions
                     options.Cookie.Name = "AdminSession";
                     options.Cookie.HttpOnly = false;
                     options.Cookie.SameSite = SameSiteMode.None;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
                     options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
                     options.SlidingExpiration = true;
                     options.Events.OnRedirectToLogin = context =>
